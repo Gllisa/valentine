adfe34e7b1e2859648a2b9fd37115d4b9332c0ce
Lilber <emaksishkin@mail.ru>
2022-01-31 14:33:04 +0300

    new commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-"use strict";
-let squares = 1000;
-let container = document.getElementById("container");
-for (let i = 0; i < squares; i++) {
-  let square = document.createElement("div");
-  square.classList.add("square");
-  square.addEventListener("mouseover", function (event) {});
-  square.addEventListener("mouseout", function (event) {});
-  container.appendChild(square); //Uncaught TypeError: Cannot read properties of null (reading 'appendChild').
-}
+
