62cabe242ed6e74bea10f634e7afec23ac4a8ec8
Lilber <emaksishkin@mail.ru>
2022-01-31 14:20:59 +0300

    create files and first jov

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+let squares = 1000;
+let container = document.getElementById("container");
+for (let i = 0; i < squares; i++) {
+  let square = document.createElement("div");
+  square.classList.add("square");
+  square.addEventListener("mouseover", function (event) {});
+  square.addEventListener("mouseout", function (event) {});
+  container.appendChild(square);
+}
